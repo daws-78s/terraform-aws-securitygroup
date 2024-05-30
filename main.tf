985bf79851f7d249a792461293310360dea84867
sivakumar <info@joindevops.com>
2024-05-30 11:29:32 +0530

    sg

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ resource "aws_security_group" "allow_tls" {
   dynamic "ingress" {
     for_each = var.ingress_rules
     content {
-        from_port       = egress.value["from_port"]
-        to_port         = egress.value["to_port"]
-        protocol        = egress.value["protocol"]
-        cidr_blocks = egress.value["cidr_blocks"]
+        from_port       = ingress.value["from_port"]
+        to_port         = ingress.value["to_port"]
+        protocol        = ingress.value["protocol"]
+        cidr_blocks = ingress.value["cidr_blocks"]
     }
   }
 
